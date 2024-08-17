8d1f7fc1c092884705288c9ce7fc4a1663f247b5
tanchelka11 <tetiana.rens@gmail.com>
2024-08-17 12:24:31 -0400

    Finished day 5

---
+++ day_5_max_num.py
@@ -0,0 +1,8 @@
+scores = [100, 101, 99, 124, 100, 95, 112]
+
+max_score = 0
+for score in scores:
+    if score > max_score:
+        max_score = score
+
+print(max_score)
---
+++ day_5_password_generator.py
@@ -0,0 +1,32 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+num_letters = int(input("How many letters would you like in your password? "))
+num_symbols = int(input("How many symbols would you like in your password? "))
+num_numbers = int(input("How many numbers would you like in your password? "))
+
+password_lst = []
+for num in range(num_letters):
+    rand_letter = random.choice(letters)
+    password_lst.append(rand_letter)
+
+for num in range(num_symbols):
+    rand_symbol = random.choice(symbols)
+    password_lst.append(rand_symbol)
+
+for num in range(num_numbers):
+    rand_number = random.choice(numbers)
+    password_lst.append(rand_number)
+
+random.shuffle(password_lst)
+
+password = ""
+for char in password_lst:
+    password += char
+print(password)
